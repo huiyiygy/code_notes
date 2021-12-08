c0e60d1d0836da26fdf7876389e2ad9d2efd16f0
huiyiygy <huiyiygy@aliyun.com>
2021-12-09 01:07:27 +0800

    update leetcode 0002

--- LeetCode/code/0002_Add_Two_Numbers/0002_Add_Two_Numbers.cpp
+++ LeetCode/code/0002_Add_Two_Numbers/0002_Add_Two_Numbers.cpp
@@ -4,79 +4,108 @@
 原因：342 + 465 = 807
 */
 #include<iostream>
+#include <memory>
+#include <vector>
 
 using namespace std;
 
+// 单链表
 struct ListNode {
 	int val;
 	ListNode *next;
-	ListNode(int x) : val(x), next(NULL) {}
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct ListNode_cpp {
+    int val;
+    std::shared_ptr<ListNode_cpp> next;
+    ListNode_cpp() : val(0), next(nullptr) {}
+    ListNode_cpp(int x) : val(x), next(nullptr) {}
+    ListNode_cpp(int x, std::shared_ptr<ListNode_cpp> other) : val(x), next(other) {}
 };
 
 class Solution {
 public:
 	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-		ListNode* result = new ListNode(0);
-		ListNode* next = result;
-		int carry = 0; //进位数
-		int var_a, var_b, var;
+		ListNode* head = new ListNode(0);
+		ListNode* result = head;
+		//  var_a var_b 两个输入的值，sum 两数和，carry 进位数
+		int var_a = 0, var_b = 0, sum = 0, carry = 0;
 		while (true) {
-			var_a = l1 == NULL ? 0 : (*l1).val;
-			var_b = l2 == NULL ? 0 : (*l2).val;
-			var = var_a + var_b + carry;
-			if (var >= 10) {
-				var = var % 10;
+			var_a = l1 == nullptr ? 0 : l1->val;
+			var_b = l2 == nullptr ? 0 : l2->val;
+			sum = var_a + var_b + carry;
+			if (sum >= 10) {
+				sum %= 10;
 				carry = 1;
 			}
 			else {
 				carry = 0;
 			}
-			(*next).next = new ListNode(var);
-			next = (*next).next;
-			l1 = l1 == NULL ? NULL : (*l1).next;
-			l2 = l2 == NULL ? NULL : (*l2).next;
-			if (l1 == NULL && carry == 0){
-				(*next).next = l2;
+			result->next = new ListNode(sum);
+			result = result->next;
+			l1 = l1 == nullptr ? nullptr : l1->next;
+			l2 = l2 == nullptr ? nullptr : l2->next;
+			if (l1 == nullptr && carry == 0){
+				result->next = l2;
 				break;
 			}
-			if (l2 == NULL && carry == 0){
-				(*next).next = l1;
+			else if (l2 == nullptr && carry == 0){
+				result->next = l1;
 				break;
 			}
 		}
-		return (*result).next;
+		return head->next;
 	}
 };
 
 int main(){
 	//构造两个链表
-	int nums1[3] = { 2, 4, 3 };
-	int nums2[3] = { 5, 6, 4 };
-	ListNode* l1 = new ListNode(0); //表头
-	ListNode* l2 = new ListNode(0);
-	ListNode* l1_next = l1; //尾指针
-	ListNode* l2_next = l2;
-	for (size_t i = 0; i < 3; i++)
-	{
-		(*l1_next).next = new ListNode(nums1[i]);
-		l1_next = (*l1_next).next; //移动尾指针
-		(*l2_next).next = new ListNode(nums2[i]);
-		l2_next = (*l2_next).next;
-	}
-	ListNode* p1 = l1;
-	ListNode* p2 = l2;
-	l1 = (*l1).next; //移除空表头
-	l2 = (*l2).next;
-	free(p1);
-	free(p2);
+	vector<int> m = {9, 8, 7, 6, 5, 4, 3};
+	vector<int> n = {0, 1, 2};  // result = {9, 9, 9, 6, 5, 4, 3}
+	ListNode *input_1 = nullptr, *input_2 = nullptr, *result = nullptr, *temp = nullptr;
+    // 数组逆序遍历，初始化输入链表
+    for (int i = m.size() -1; i >=0 ; i--)
+    {
+        input_1 = new ListNode(m[i], input_1);
+    }
+    for (int i = n.size() -1; i >=0 ; i--)
+    {
+        input_2 = new ListNode(n[i], input_2);
+    }
 
 	Solution s;
-	ListNode* result = s.addTwoNumbers(l1, l2);
+	result = s.addTwoNumbers(input_1, input_2);
 
-	while (result != NULL){
-		cout << (*result).val << "  ";
-		result = (*result).next;
+	// 打印结果
+	while (result != nullptr){
+		cout << result->val << "  ";
+		result = result->next;
 	}
+	// 释放手动申请的内存
+    while(input_1 != nullptr)
+    {
+        temp = input_1->next;
+        delete input_1;
+        input_1 = nullptr;
+        input_1 = temp;
+    }
+    while(input_2 != nullptr)
+    {
+        temp = input_2->next;
+        delete input_2;
+        input_2 = nullptr;
+        input_2 = temp;
+    }
+    while(result != nullptr)
+    {
+        temp = result->next;
+        delete result;
+        result = nullptr;
+        result = temp;
+    }
 
 	system("pause");
 	return 0;
